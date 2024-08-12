b0c6a5e3a1cd209c1cf57ba45ba99ef666135c23
Matheus Assunção <130656978+Matheus-Assuncao@users.noreply.github.com>
2024-08-12 13:52:51 -0300

    12/08 arquivo 1

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,18 @@
+-- TABLE
+CREATE TABLE campo(
+	id int(4),
+  	cidade varchar(20) UNIQUE,
+  	capacidade int(4) NOT NULL
+);
+CREATE TABLE jogador(
+	id int(4),
+  	nome varchar(30),
+  	esporte varchar(30)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
