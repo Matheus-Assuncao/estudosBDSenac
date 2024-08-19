9c971b17e5b52adeb117c7a8f11ed7fd9c797ac3
Matheus Assunção <130656978+Matheus-Assuncao@users.noreply.github.com>
2024-08-19 15:55:55 -0300

    atividade 19/03
    Atividade DQL

---
+++ dump.sqlite (3).sql
@@ -0,0 +1,23 @@
+-- TABLE
+CREATE TABLE curiosidades_paris2024(
+ id integer PRIMARY key AUTOINCREMENT,
+ curiosidade TEXT not NULL,
+ categoria TEXT
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogos_esportivos(
+  id integer PRIMARY key AUTOINCREMENT,
+  esporte varchar(100),
+  equipe_casa varchar(100),
+  equipe_visitante varchar(100),
+  pontuacao_casa int (100),
+  pontuacao_visitante int (100),
+  data_jogo DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
