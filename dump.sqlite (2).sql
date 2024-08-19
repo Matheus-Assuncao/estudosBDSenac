691670255022658c7b4445267080632422172f77
Matheus Assunção <130656978+Matheus-Assuncao@users.noreply.github.com>
2024-08-19 14:01:14 -0300

    aula 19/08

---
+++ dump.sqlite (2).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE produtos(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  nome varchar(100) NOT NULL,
+  descricao text,
+  preco decimal(10,2),
+  dataHora DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
